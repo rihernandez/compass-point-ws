e8271b359174f02edde5a2f35a400dfaa89065d7
rihernandez <rhc921004@gmail.com>
2022-11-04 11:24:18 -0400

    chore: sdsd

--- src/redis.ts
+++ src/redis.ts
@@ -3,10 +3,10 @@ import { createClient } from 'redis';
 
 const client = createClient({
     socket: {
-        host: 'redis-17429.c257.us-east-1-3.ec2.cloud.redislabs.com',
+        host: process.env.REDIS_ENDPOINT_URI || 'redis-17429.c257.us-east-1-3.ec2.cloud.redislabs.com',
         port: parseInt('17429')
     },
-    password: 'IKQP6YO7KhkQCbqksSUj3pmA06LuizG9'
+    password: process.env.REDIS_PASSWORD || 'IKQP6YO7KhkQCbqksSUj3pmA06LuizG9'
   });
 
 
